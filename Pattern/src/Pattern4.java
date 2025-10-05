fbf56dbe1fbd96d64585cba90daa2e21858e24b1
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-10-05 23:31:37 +0530

    Add files via upload

---
+++ Pattern/src/Pattern4.java
@@ -0,0 +1,27 @@
+
+public class Pattern4 {
+	
+	public static void main(String[] args) {
+		
+		
+	int n = 4 ;
+	
+	
+	for(int i  = 0 ; i <= n ; i ++)
+	{
+		for(int j = 0 ; j <= n  ; j ++ )
+		{
+			if(j <= n - i )
+			{
+				System.out.print(" ");
+			}
+			else
+			{
+				System.out.print("*");
+			}
+		}
+		System.out.println();
+	}
+	}
+
+}
